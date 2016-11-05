f6d943383b1d6b659dbcb687e3a632019b6700a0
vincent-heng <hengvincent.1994@gmail.com>
2016-11-05 12:17:15 +0100

    Weapon OK

--- Assets/scripts/WeaponB.cs
+++ Assets/scripts/WeaponB.cs
@@ -13,13 +13,13 @@ public class WeaponB : MonoBehaviour {
 		// TODO if touching a mob, kill it
 
 		// Get all mobs
-		Monster[] monsters = GameObject.FindObjectsOfType(typeof(Monster)) as Monster[];
-		foreach (Monster monster in monsters) {
+		AbstractMonster[] monsters = GameObject.FindObjectsOfType(typeof(AbstractMonster)) as AbstractMonster[];
+		foreach (AbstractMonster monster in monsters) {
 			// Is touching ?
-			/*float dist = Vector3.Distance(transform.position, monster.gameObject.transform.position);
+			float dist = Vector3.Distance(transform.position, monster.gameObject.transform.position);
 			if (dist < 1) { // Touching = under 1 distance unit
-				monster.die();
-			}*/
+				monster.Die();
+			}
 		}
 			
 
