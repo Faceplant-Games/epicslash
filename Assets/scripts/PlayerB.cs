401e3088a064c96a6d9c0617801d2847a702bbb4
vincent-heng <hengvincent.1994@gmail.com>
2016-11-05 11:33:39 +0100

    Weapon and Player

---
+++ Assets/scripts/PlayerB.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerB : MonoBehaviour {
+
+	int level = 0;
+	WeaponB weaponB;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void equipWeapon (WeaponB weaponB) {
+		this.weaponB = weaponB;
+	}
+}
--- Assets/scripts/WeaponB.cs
+++ Assets/scripts/WeaponB.cs
@@ -10,6 +10,19 @@ public class WeaponB : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// TODO if touching a mob, kill it
+
+		// Get all mobs
+		GameObject[] monsters = GameObject.FindObjectsOfType<Monster>();
+		foreach (GameObject monster in monsters) {
+			// Is touching ?
+			float dist = Vector3.Distance(transform.position, monster.transform.position);
+			if (dist < 1) { // Touching = under 1 distance unit
+				// TODO : monster.die();
+			}
+		}
+			
+
+
 	}
 }
