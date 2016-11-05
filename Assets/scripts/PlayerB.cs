fc68533f3565658b7480bc53578dd36e44ec93cb
vincent-heng <hengvincent.1994@gmail.com>
2016-11-05 21:56:01 +0100

    GoldSpawners

--- Assets/scripts/PlayerB.cs
+++ Assets/scripts/PlayerB.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerB : MonoBehaviour {
 
 	int level;
 	int stage;
 	long[] treshs = { 100, 100000, 100000000, 100000000000, 100000000000000 };
+	public List<GoldSpawnerB> goldSpawners = new List<GoldSpawnerB>();
     GameManager gm;
 
     WeaponB weaponB;
@@ -24,6 +26,7 @@ public class PlayerB : MonoBehaviour {
 
 	public void levelUp(int levels) {
 		level += levels;
+		spawnGold(levels*3);
         print(level);
 		if (level >= treshs [stage]) {
 			stage++;
@@ -34,6 +37,11 @@ public class PlayerB : MonoBehaviour {
 				//stop the game
 			}	
 		}
+	}
+
+	public void spawnGold(int levels) {
+		// random on goldSpawners.length, to pop some gold bags
+		goldSpawners[Random.Range (0, goldSpawners.Count)].Spawn(levels);
 	}
 
 	public void levelDown(int levels) {
