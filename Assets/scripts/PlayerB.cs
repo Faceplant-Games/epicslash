5d26c05c536712ff11f0bb547554c3c416e3571c
Nope-Nope-Nope\Acer <francois.vanderperre@gmail.com>
2016-11-05 16:39:55 +0100

    updated managers

--- Assets/scripts/PlayerB.cs
+++ Assets/scripts/PlayerB.cs
@@ -5,6 +5,7 @@ public class PlayerB : MonoBehaviour {
 
 	int level;
 	int stage;
+	GameManager gm = GameObject.FindObjectOfType(typeof(GameManager)) as GameManager ;
 	long[] treshs = { 100, 100000, 100000000, 100000000000, 100000000000000 };
 	WeaponB weaponB;
 
@@ -23,7 +24,9 @@ public class PlayerB : MonoBehaviour {
 		level += levels;
 		if (level >= treshs [stage]) {
 			stage++;
-			//fire event
+			gm.stage = stage;
+			gm.change = true;
+			//FIXME PAUSE
 			if (stage > treshs.Length) {
 				//stop the game
 			}	
@@ -32,13 +35,17 @@ public class PlayerB : MonoBehaviour {
 
 	void levelDown(int levels) {
 		level -= levels;
-
+		if (level >0) {
+			if ( level < treshs[stage-1] ) {
+				stage--;
+				gm.stage = stage;
+				gm.change = true;
+				//FIXME PAUSE
+			}
+		}
 	}
 
 	void equipWeapon (WeaponB weaponB) {
 		this.weaponB = weaponB;
 	}
-
-
-
 }
