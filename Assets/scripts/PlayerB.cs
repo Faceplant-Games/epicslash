56019f95fc548972b984fe281185d0bfa63d9418
Nope-Nope-Nope\Acer <francois.vanderperre@gmail.com>
2016-11-05 15:35:36 +0100

    fix

--- Assets/scripts/PlayerB.cs
+++ Assets/scripts/PlayerB.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class PlayerB : MonoBehaviour {
 
-	int level = 0;
-	long [] treshs = {  100, 100000, 100000000, 100 000 000 000, 100 000 000 000 000   }
+	int level;
+	int stage;
+	long[] treshs = { 100, 100000, 100000000, 100000000000, 100000000000000 };
 	WeaponB weaponB;
 
 	// Use this for initialization
 	void Start () {
-	
+		level = 0;
+		stage = 0;
 	}
 	
 	// Update is called once per frame
@@ -19,16 +21,24 @@ public class PlayerB : MonoBehaviour {
 
 	void levelUp(int levels) {
 		level += levels;
-
+		if (level >= treshs [stage]) {
+			stage++;
+			//fire event
+			if (stage > treshs.Length) {
+				//stop the game
+			}	
+		}
 	}
 
 	void levelDown(int levels) {
 		level -= levels;
+
 	}
 
 	void equipWeapon (WeaponB weaponB) {
 		this.weaponB = weaponB;
 	}
+
 
 
 }
