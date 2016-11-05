a97ff941dee0835ee3eff2d909a5da17fa7d1e72
vincent-heng <hengvincent.1994@gmail.com>
2016-11-05 16:03:29 +0100

    definition Bullet

---
+++ Assets/scripts/BulletB.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	float timer = 0;
+	float lifeTime = 10;
+	float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float distanceThisFrame = speed * Time.fixedDeltaTime;
+		RaycastHit hit = new RaycastHit ();
+
+		// At each frame, we cast a ray forward from where we are to where we will be next frame
+		if (Physics.Raycast (transform.position, transform.forward, out hit, distanceThisFrame)) {
+			if (hit.transform.gameObject.GetComponent<AbstractMonster>() != null) {
+				AbstractMonster monster = hit.transform.gameObject.GetComponent<AbstractMonster>();
+				monster.Die ();
+				Destroy (gameObject);
+			}
+		} else {
+			transform.position += transform.forward * distanceThisFrame;
+		}
+
+		timer += Time.fixedDeltaTime;
+		if (timer > lifeTime) {
+			Destroy(gameObject);
+		}
+	}
+
+
+}
