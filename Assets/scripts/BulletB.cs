76dd6b0580ddda9ee5fc205b5ebe607e7d56a5e3
vincent-heng <hengvincent.1994@gmail.com>
2016-11-06 00:35:28 +0100

    Fire fire fire

--- Assets/scripts/BulletB.cs
+++ Assets/scripts/BulletB.cs
@@ -25,6 +25,9 @@ public class BulletB : MonoBehaviour {
 				monster.Die ();
 				Destroy (gameObject);
 			}
+			else if (hit.transform.gameObject.GetComponent<GameObject>() != null) {
+				Destroy (gameObject);
+			}
 		} else {
 			transform.position += transform.forward * distanceThisFrame;
 		}
