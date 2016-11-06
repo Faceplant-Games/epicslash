683597a69c693d98d88e5f4d13063dc746f9c200
rLeblancTemp <rmi.remi@hotmail.fr>
2016-11-06 04:20:41 +0100

    no message

---
+++ Assets/BossAssets/Script/ExplosiveWings.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosiveWings : MonoBehaviour 
+{
+	public GameObject ExplosionFX;
+
+	private bool _explosionEnabled = false;
+	public bool ExplosionEnabled
+	{
+		get
+		{
+			return _explosionEnabled;
+		}
+		set
+		{
+			_explosionEnabled = value;
+			if (_explosionEnabled == true)
+			{
+				GetComponent<MeshRenderer>().material = GameObject.FindObjectOfType<BossFightManager>().WhiteEyeMaterial;
+			}
+			else
+			{
+				GetComponent<MeshRenderer>().material = GameObject.FindObjectOfType<BossFightManager>().BlueWingsMaterial;
+			}
+		}
+	}
+
+	private int HP = 30;
+
+	public void TriggerExplosion(Vector3 position)
+	{
+		if (ExplosionEnabled)
+		{
+			GameObject FX = Instantiate(ExplosionFX, position, Quaternion.identity) as GameObject;
+			HP --;
+			if (HP < 0)
+			{
+				ExplosionEnabled = false;
+				GetComponent<MeshRenderer>().material = GameObject.FindObjectOfType<BossFightManager>().BlueWingsMaterial;
+				GameObject.FindObjectOfType<BossFightManager>().WingsBrokenStage2 ++;
+				Destroy(gameObject);
+			}
+		}
+	}
+}
