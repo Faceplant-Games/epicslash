2cd687c3718d6d356b0529a82fb95547bd47189e
megal000l <disturbman52@gmail.com>
2016-11-06 05:47:56 +0100

    commit

--- Assets/scripts/Managers/GameManager.cs
+++ Assets/scripts/Managers/GameManager.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 	private int previousStage = 0;
 	public int stage = 0;
 	public bool change = false;
 
-
+    public AudioClip track;
 	public AudioClip ups;
 	public AudioClip downs;
 	public AudioSource audio;
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        audio.PlayOneShot(track);
 	}
 	
 	// Update is called once per frame
@@ -24,6 +26,7 @@ public class GameManager : MonoBehaviour {
 			if (stage > previousStage) {
 				print ("on monte de niveau");
 				audio.PlayOneShot (ups);
+                SceneManager.LoadScene("BossFight");
 			} else if (stage < previousStage) {
 				print ("on baisse de niveau");
 				audio.PlayOneShot (downs);
