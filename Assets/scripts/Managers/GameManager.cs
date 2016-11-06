d81c5c707ef15edf7128a20a1447258ec0e1c874
Nope-Nope-Nope\Acer <francois.vanderperre@gmail.com>
2016-11-06 04:44:30 +0100

    level sounds

--- Assets/scripts/Managers/GameManager.cs
+++ Assets/scripts/Managers/GameManager.cs
@@ -6,15 +6,6 @@ public class GameManager : MonoBehaviour {
 	public int stage = 0;
 	public bool change = false;
 
-	public AudioSource theme1;
-	public AudioSource theme1to2;
-	public AudioSource theme2;
-	public AudioSource theme2to3;
-	public AudioSource theme3;
-	public AudioSource theme3to4;
-	public AudioSource theme4;
-	public AudioSource theme4to5;
-	public AudioSource theme5;
 
 	public AudioSource ups;
 	public AudioSource downs;
@@ -23,17 +14,19 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		theme1.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (change) {
 			change = false;
-			if (stage > previousStage)
+			if (stage > previousStage) {
 				print ("on monte de niveau");
-			else if (stage < previousStage)
+				ups.PlayOneShot ();
+			} else if (stage < previousStage) {
 				print ("on baisse de niveau");
+				downs.PlayOneShot ();
+			}
 			previousStage = stage;
 		}
 	}
