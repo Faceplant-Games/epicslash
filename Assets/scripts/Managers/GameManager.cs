1c7bab170ccf691fcf92a52aa09e012cc345c0a6
vincent-heng <hengvincent.1994@gmail.com>
2016-11-10 14:32:41 +0100

    First commit since jam 0.5-SNAPSHOT

--- Assets/scripts/Managers/GameManager.cs
+++ Assets/scripts/Managers/GameManager.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 	private int previousStage = 0;
@@ -9,30 +8,31 @@ public class GameManager : MonoBehaviour {
 	public bool change = false;
 
     public AudioClip track;
-	public AudioClip ups;
+    public AudioClip loopTrack;
+    public AudioClip ups;
 	public AudioClip downs;
-	public AudioSource audio;
+	public AudioSource audioSource;
 
 
 
 	// Use this for initialization
-	void Start () {
-		audio.PlayOneShot(track);
+	void Start ()
+    {
+        InitializeTrack();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 		if (change) {
 			change = false;
 			if (stage > previousStage) {
 				print ("on monte de niveau");
-				audio.PlayOneShot (ups);
+				audioSource.PlayOneShot (ups);
                 //SceneManager.LoadScene("BossFight");
 				SceneManager.LoadScene(stage ++);
 			} else if (stage < previousStage) {
 				print ("on baisse de niveau");
-				audio.PlayOneShot (downs);
+				audioSource.PlayOneShot (downs);
 				if (stage > 0) 
 				{
 					SceneManager.LoadScene(stage - 1);
@@ -43,8 +43,25 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-	//catch events change of stage
+    //catch events change of stage
 
+    private void InitializeTrack()
+    {
+        audioSource.PlayOneShot(track);
+        if (loopTrack == null)
+        {
+            audioSource.loop = true;
+            return;
+        }
+        audioSource.loop = false;
+
+        AudioSource loopAudio = new AudioSource();
+        loopAudio.transform.position = audioSource.transform.position;
+        GameObject.Instantiate(loopAudio);
+        loopAudio.PlayScheduled(track.length);
+        loopAudio.PlayOneShot(loopTrack);
+        loopAudio.loop = true;
+    }
 
 
 }
