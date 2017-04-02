a089c967259f64ff69ac3c0beb5ca29adfb8db7f
vincent-heng <vincentheng@users.noreply.github.com>
2017-04-02 19:01:33 +0200

    FIXME added on a deprecated method

--- Assets/scripts/Managers/Fading.cs
+++ Assets/scripts/Managers/Fading.cs
@@ -35,7 +35,7 @@ public class Fading : MonoBehaviour {
     }
 
     // OnLevelWasLoaded is called when a level is loaded. It takes loaded level index (int) as a parameter so you can limit the fade in to certain scenes
-    void OnLevelWasLoaded()
+    void OnLevelWasLoaded() // FIXME deprecated method, use OnLevelFinishedLoading instead.
     {
         // alpha = 1; use this if the alpha ois not set to 1 by default
         BeginFade(-1);
