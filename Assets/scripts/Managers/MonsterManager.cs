70f548ab995fd063d5dd1dc406400e5417d8236e
vincent-heng <hengvincent.1994@gmail.com>
2017-10-28 12:34:00 +0200

    Clean Monster Manager

--- Assets/scripts/Managers/MonsterManager.cs
+++ Assets/scripts/Managers/MonsterManager.cs
@@ -12,69 +12,69 @@ using System.Collections.Generic;
 /// <seealso cref="GameManager"/>
 /// <seealso cref="SpawnerB"/>
 public class MonsterManager : MonoBehaviour {
-	private List<SpawnerB> spawns = new List<SpawnerB> ();
-	private List<string> [] ennemies;
+	private List<SpawnerB> spawners = new List<SpawnerB> ();
+	private List<string> [] mobTypesByStage; // TODO Revrieve it from config file
 	private GameManager gm;
+    private int numberOfStages = 5;
 
 	void Start () {
+        gm = gameObject.GetComponent<GameManager>();
 
-		gm = gameObject.GetComponent<GameManager>();
-		ennemies  = new List<string>[5]; // TODO rename this variable "type of enemies spawned in some level" or something like this
-		for (int i = 0; i < 4; i++) {
-			ennemies[i] =  new List<string>();
-		}
-		ennemies [0].Add ("Monster1"); // TODO Put this in a config file
+        InitializeMobTypesByStage();
 
-        ennemies [1].Add ("Monster1");
-		ennemies [1].Add ("Monster2");
-		ennemies [1].Add ("Monster3");
+        SpawnerB[] spawners = GameObject.FindObjectsOfType(typeof(SpawnerB)) as SpawnerB[];
 
-		ennemies [2].Add ("Monster1");
-		ennemies [2].Add ("Monster2");
-		ennemies [2].Add ("Monster3");
-		ennemies [2].Add ("dragon");
+        foreach (SpawnerB spawner in spawners) { // TODO use lambdas, it's sexier : Array.ForEach(spawners, s => spawns.Add(s));
+            this.spawners.Add(spawner); // TODO And... is it really useful?
+        }
+    }
 
-        ennemies[3].Add("Monster1");
-        ennemies[3].Add("Monster2");
-        ennemies[3].Add("Monster3");
-        ennemies[3].Add("dragon");
-
-      //  ennemies [4].Add ("");
-
-		SpawnerB[] spawnerB = GameObject.FindObjectsOfType (typeof(SpawnerB)) as SpawnerB[]; // TODO rename this clearer
-
-
-
-		foreach (SpawnerB spawner in spawnerB) { // TODO use lambdas, it's sexier : Array.ForEach(spawners, s => spawns.Add(s));
-			spawns.Add(spawner); // TODO And... is it really useful?
+    // Update is called once per frame
+    void Update () {
+        if (spawners.Count == 0) {
+            return;
+        }
+		if (Random.Range (0, 10) < 1) { // This is the frame rate. (0,10) < 1 means 10%
+            if (GameObject.FindObjectsOfType(typeof(AbstractMonster)).Length < 100) {
+                SpawnMob();
+            }
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (spawns.Count > 0) {
-			if (Random.Range (0, 10) < 1) {// changer ça pour modifier le spawn rate
-				if (GameObject.FindObjectsOfType (typeof(AbstractMonster)).Length < 100)
-					Spawn ();
-			}
-		}
+    
+    private void InitializeMobTypesByStage() {
+        mobTypesByStage = new List<string>[numberOfStages];
+
+        for (int i = 0; i < numberOfStages; i++) {
+            mobTypesByStage[i] = new List<string>();
+        }
+        mobTypesByStage[0].Add("Monster1"); // TODO Retrieve it from config file
+
+        mobTypesByStage[1].Add("Monster1");
+        mobTypesByStage[1].Add("Monster2");
+        mobTypesByStage[1].Add("Monster3");
+
+        mobTypesByStage[2].Add("Monster1");
+        mobTypesByStage[2].Add("Monster2");
+        mobTypesByStage[2].Add("Monster3");
+        mobTypesByStage[2].Add("dragon");
+
+        mobTypesByStage[3].Add("Monster1");
+        mobTypesByStage[3].Add("Monster2");
+        mobTypesByStage[3].Add("Monster3");
+        mobTypesByStage[3].Add("dragon");
+    }
+
+    // Spawn 1 mob at a random spawner
+    void SpawnMob() {
+		spawners[Random.Range (0, spawners.Count)].Spawn(this.ChooseRandomMobType());
 	}
 
 
-
-	void Spawn(){ // TODO rename it spawnMob
-		// Find a random index between zero and one less than the number of spawn points.
-		spawns[Random.Range (0, spawns.Count)].Spawn(this.findEnnemyTag());
-
-	}
-
-    // TODO rename this method "Give a random ennemi of this stage" or something like this
-	string findEnnemyTag(){ //FIXME ajouter les differents types de monstres
-
+    string ChooseRandomMobType() {
 		int stage = gm.stage;
-		List<string> spawnsrattttt = ennemies [stage]; // Name this clearer
-		int r = Random.Range (0, 12); // FIXME why 12?
-		return spawnsrattttt [r % (spawnsrattttt.Count)];
+		List<string> mobTypes = mobTypesByStage [stage];
+		int r = Random.Range (0, mobTypes.Count);
+		return mobTypes [r % (mobTypes.Count)];
 	}
 
 }
